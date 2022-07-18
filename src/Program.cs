2320f22441e605d44e35eb2e39e812485301be08
Cesare Caoduro <cesare.caoduro@gmail.com>
2022-07-18 13:21:46 +1000

    fixed IfcProject causing file to be corrupted

--- src/Program.cs
+++ src/Program.cs
@@ -1,21 +1,18 @@
 ﻿using GeometryGym.Ifc;
 
 var db = new DatabaseIfc(ReleaseVersion.IFC4X3_RC4);
-var site = new IfcSite(db, "MySite")
-{
-    Description = "Just another site",
-};
+
 
 
 var facility = new IfcFacility(db, "MyFacility");
 
-var bridge = new IfcBridge(db)
+var bridge = new IfcBridge(facility.Database)
 {
     Name = "MyBridge",
     Description = "Just another bridge"
 };
 
-var project = new IfcProject(site, "MyProject", IfcUnitAssignment.Length.Metre);
+var project = new IfcProject(bridge, "MyProject", IfcUnitAssignment.Length.Metre);
 {
 };
 
@@ -29,8 +26,8 @@ var foundations = new IfcFacilityPart(
 
 var name = "Pile Cap";
 var length = 1.2;
-var width = 1.2;
-var depth = 2.4;
+var width = 2.4;
+var depth = 4.8;
 
 IfcMaterial material = new IfcMaterial(db, "Concrete")
 {
@@ -41,7 +38,7 @@ IfcFootingType footingType = new IfcFootingType(db, name, IfcFootingTypeEnum.PAD
     MaterialSelect = material,
 };
 
-IfcRectangleHollowProfileDef rect = new IfcRectangleHollowProfileDef(db, name, length, width, depth);
+IfcRectangleProfileDef rect = new IfcRectangleProfileDef(db, "rect", length, width);
 IfcExtrudedAreaSolid extrusion = new IfcExtrudedAreaSolid(rect, new IfcAxis2Placement3D(new IfcCartesianPoint(db, 0, 0, 0)), new IfcDirection(db, 0, 0, 1), depth);
 
 IfcProductDefinitionShape productRep = new IfcProductDefinitionShape(new IfcShapeRepresentation(extrusion));
@@ -65,3 +62,22 @@ IfcFooting footing = new(
 };
 
 db.WriteFile(Path.Combine("IFC4X3RC4_testBridge.ifc"));
+
+//DatabaseIfc db = new DatabaseIfc(ModelView.Ifc4DesignTransfer);
+//IfcBuilding building = new IfcBuilding(db, "IfcBuilding") { };
+//IfcProject project = new IfcProject(building, "IfcProject", IfcUnitAssignment.Length.Millimetre) { };
+
+////IfcBuildingStorey storey = new IfcBuildingStorey(building, "Ground Floor", 0);
+//IfcMaterial masonryFinish = new IfcMaterial(db, "Masonry - Brick - Brown");
+//IfcMaterial masonry = new IfcMaterial(db, "Masonry");
+//IfcMaterialLayer layerFinish = new IfcMaterialLayer(masonryFinish, 110, "Finish");
+//IfcMaterialLayer airInfiltrationBarrier = new IfcMaterialLayer(db, 50, "Air Infiltration Barrier") { IsVentilated = IfcLogicalEnum.TRUE };
+//IfcMaterialLayer structure = new IfcMaterialLayer(masonry, 110, "Core");
+//string name = "Double Brick - 270";
+//IfcMaterialLayerSet materialLayerSet = new IfcMaterialLayerSet(new List<IfcMaterialLayer>() { layerFinish, airInfiltrationBarrier, structure }, name);
+//IfcMaterialLayerSetUsage materialLayerSetUsage = new IfcMaterialLayerSetUsage(materialLayerSet, IfcLayerSetDirectionEnum.AXIS2, IfcDirectionSenseEnum.POSITIVE, 0);
+//db.NextObjectRecord = 300;
+//IfcWallType wallType = new IfcWallType(name, materialLayerSet, IfcWallTypeEnum.NOTDEFINED) { };
+//IfcWallStandardCase wallStandardCase = new IfcWallStandardCase(building, materialLayerSetUsage, new IfcAxis2Placement3D(new IfcCartesianPoint(db, 0, 0, 0)), 5000, 2000) { };
+
+//db.WriteFile(Path.Combine("IFC4_testWall.ifc"));
